24d863a528e966fd69753a8ba66a66c3b7e12463
Felipe Torres <ftorres@brex.com>
2022-05-22 17:14:59 -0700

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    'NEXT_PUBLIC_APP_ENV': process.env.APP_ENV
+    'NEXT_PUBLIC_APP_ENV': process.env.APP_ENV,
+    'NEXT_PUBLIC_APP_VERSION': process.env.CF_PAGES_COMMIT_SHA || "local"
   },
   experimental: {
     emotion: true,
