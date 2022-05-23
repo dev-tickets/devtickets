635f46f663f3618f1aff030f65f5ffe09af1acc1
Felipe Torres <ftorres@brex.com>
2022-05-22 17:15:37 -0700

    update

--- src/Features/Sentry/index.tsx
+++ src/Features/Sentry/index.tsx
@@ -1,9 +1,9 @@
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
-console.log("process.env.NEXT_PUBLIC_APP_ENV", process.env.NEXT_PUBLIC_APP_ENV);
 Sentry.init({
   environment: process.env.NEXT_PUBLIC_APP_ENV,
+  release: process.env.NEXT_PUBLIC_APP_VERSION,
   dsn: "https://ba3f592af9f1452c8180c2334f757402@o255017.ingest.sentry.io/6432025",
   integrations: [new BrowserTracing()],
   // Set tracesSampleRate to 1.0 to capture 100%
