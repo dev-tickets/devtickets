517aba2721bb55c0510ebfe3e5bfe8910060129b
Felipe Torres <ftorres@brex.com>
2022-07-13 10:09:22 -0700

    update layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,50 +3,18 @@ import "@fontsource/work-sans/700.css"; // Weight 700.
 
 import "@fontsource/work-sans/variable-italic.css"; // Italic variant.
 import "@fontsource/work-sans/variable.css"; // Contains ONLY variable weights and no other axes.
+import { TopBarLayout } from "@/components/ApplicationLayouts/TopBarLayout";
 
 import { useApolloClient } from "@/features/Apollo";
 import { AuthProvider } from "@/features/Auth/supabase";
 import "@/features/Sentry";
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
-import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { ReactElement, ReactNode, useEffect, useState } from "react";
 import React from "react";
 import { theme } from "../src/Styling/theme";
 
-type NextPageWithLayout = NextPage & {
-  getLayout: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-// eslint-disable-next-line react/display-name
-const AfterAuthComponent = ({
-  getLayout,
-  pageProps,
-  Component,
-}: {
-  getLayout: (page: ReactElement) => ReactNode;
-  Component: NextPageWithLayout;
-  pageProps: any;
-}) => {
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    // TODO: Set "Loading" UI here
-    return <></>;
-  }
-
-  return <>{getLayout(<Component {...pageProps} />)}</>;
-};
-
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+function MyApp({ Component, pageProps }: AppProps) {
   const { apolloClient } = useApolloClient();
   return (
     <ChakraProvider
@@ -61,15 +29,9 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       <CSSReset />
       <AuthProvider>
         <ApolloProvider client={apolloClient}>
-          {typeof window === "undefined"
-            ? null
-            : (
-              <AfterAuthComponent
-                pageProps={pageProps}
-                getLayout={Component.getLayout}
-                Component={Component}
-              />
-            )}
+          <TopBarLayout>
+            <Component {...pageProps} />
+          </TopBarLayout>
         </ApolloProvider>
       </AuthProvider>
     </ChakraProvider>
@@ -77,3 +39,84 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 }
 
 export default MyApp;
+
+// import "@fontsource/work-sans/400.css"; // Weight 400.
+// import "@fontsource/work-sans/700.css"; // Weight 700.
+
+// import "@fontsource/work-sans/variable-italic.css"; // Italic variant.
+// import "@fontsource/work-sans/variable.css"; // Contains ONLY variable weights and no other axes.
+// import { TopBarLayout } from "@/components/ApplicationLayouts/TopBarLayout";
+
+// import { useApolloClient } from "@/features/Apollo";
+// import { AuthProvider } from "@/features/Auth/supabase";
+// import "@/features/Sentry";
+// import { ApolloProvider } from "@apollo/client";
+// import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+// import type { NextPage } from "next";
+// import type { AppProps } from "next/app";
+// import { ReactElement, ReactNode, useEffect, useState } from "react";
+// import React from "react";
+// import { theme } from "../src/Styling/theme";
+
+// type NextPageWithLayout = NextPage & {
+//   getLayout: (page: ReactElement) => ReactNode;
+// };
+
+// type AppPropsWithLayout = AppProps & {
+//   Component: NextPageWithLayout;
+// };
+
+// // eslint-disable-next-line react/display-name
+// const AfterAuthComponent = ({
+//   getLayout,
+//   pageProps,
+//   Component,
+// }: {
+//   getLayout: (page: ReactElement) => ReactNode;
+//   Component: NextPageWithLayout;
+//   pageProps: any;
+// }) => {
+//   const [isMounted, setIsMounted] = useState(false);
+//   useEffect(() => {
+//     setIsMounted(true);
+//   }, []);
+
+//   if (!isMounted) {
+//     // TODO: Set "Loading" UI here
+//     return <></>;
+//   }
+
+//   return <>{getLayout(<Component {...pageProps} />)}</>;
+// };
+
+// function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+//   const { apolloClient } = useApolloClient();
+//   return (
+//     <ChakraProvider
+//       theme={theme}
+//       colorModeManager={{
+//         ssr: true,
+//         type: "localStorage",
+//         set: () => "dark",
+//         get: () => "dark",
+//       }}
+//     >
+//       <CSSReset />
+//       <AuthProvider>
+//         <ApolloProvider client={apolloClient}>
+//           {typeof window === "undefined"
+//             ? null
+//             : (
+//               <AfterAuthComponent
+//                 pageProps={pageProps}
+//                 getLayout={Component.getLayout}
+//                 Component={Component}
+//               />
+//             )}
+//         </ApolloProvider>
+//       </AuthProvider>
+//     </ChakraProvider>
+//   );
+// }
+
+// export default MyApp;
--- src/Components/ApplicationLayouts/TopBarLayout/index.tsx
+++ src/Components/ApplicationLayouts/TopBarLayout/index.tsx
@@ -1,104 +1,25 @@
-import { JSConfChileIcon } from "@/components/Icons";
-import { useIsAuthenticated } from "@/features/Auth/supabase";
-import {
-  Avatar,
-  Button,
-  chakra,
-  Flex,
-  useColorModeValue,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { useViewportScroll } from "framer-motion";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { ReactNode, Suspense } from "react";
-import { dashboardMainURL } from "src/config";
-import { DesktopNavContent } from "./DesktopNavigation";
-import { useGeTopBarUserInformationQuery } from "./geTopBarUserInformation.generated";
-import { useGetUserProfile } from "./hooks";
-import { MobileNavContent } from "./MobileNav";
-import { settingsMenuLinks, unauthenticatedSettingsMenuLinks } from "./routes";
-import { SubMenuTrigger } from "./subMenu";
+import dynamic from "next/dynamic";
+import React, { ReactNode } from "react";
+import { Suspense } from "react";
 
-const ActualLayout = (
-  { children, isAuthenticated }: {
-    children: ReactNode;
-    isAuthenticated: boolean;
-  },
-) => {
-  const ref = React.useRef<HTMLElement>();
-  const [y, setY] = React.useState(0);
-  const { height = 0 } = ref.current
-    ? ref.current!.getBoundingClientRect()
-    : {};
+const ActualLayout = dynamic(() => import("./ActualLayout"), {
+  suspense: true,
+  ssr: false,
+});
 
-  const { scrollY } = useViewportScroll();
-  React.useEffect(() => {
-    return scrollY.onChange(() => setY(scrollY.get()));
-  }, [scrollY]);
-  const mobileNavDisclosure = useDisclosure();
-  const { avatarURL } = useGetUserProfile();
-  const results = useGeTopBarUserInformationQuery({
-    fetchPolicy: "network-only", // Used for first execution
-    nextFetchPolicy: "cache-only", // Used for subsequent executions
-  });
-
-  const canSeeAdminSection = React.useMemo(() => {
-    return true;
-  }, []);
-
-  return (
-    <>
-      <chakra.header
-        position="sticky"
-        top={0}
-        zIndex={100}
-        shadow={y > height ? "sm" : undefined}
-        transition="box-shadow 0.2s"
-        bg="jsconfBlack"
-        borderWidth={0}
-        w="full"
-      >
-        <chakra.div h="5rem" mx="auto" maxW="1200px">
-          <Flex
-            w="full"
-            h="full"
-            px="6"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Link
-              href={dashboardMainURL}
-              passHref
-            >
-              <Flex as="a" align="flex-start">
-                <JSConfChileIcon w={7} h={7} />
-              </Flex>
-            </Link>
-
-            <SubMenuTrigger
-              buttonContent={<Avatar size="xs" src={avatarURL} />}
-              subMenuItems={isAuthenticated
-                ? settingsMenuLinks
-                : unauthenticatedSettingsMenuLinks}
-            />
-          </Flex>
-        </chakra.div>
-      </chakra.header>
-      {children}
-    </>
-  );
-};
 /** This is only for authenticated users */
 export const TopBarLayout = React.memo(
   function TopBarLayout({ children }: { children: ReactNode }) {
     // This is an authenticated-only layout, so first we check if the user is
     // authenticated If they are not, we send them to login
-    const isAuthenticated = useIsAuthenticated();
     return (
       // <Suspense fallback={null}>
-      <ActualLayout isAuthenticated={isAuthenticated}>{children}</ActualLayout>
-      // </Suspense>
+      <>
+        <Suspense>
+          <ActualLayout />
+        </Suspense>
+        {children}
+      </>
     );
   },
 );
