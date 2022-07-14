55576d3e108c758061e43ceb9a0decf599344f05
Felipe Torres <ftorres@brex.com>
2022-07-14 00:13:30 -0700

    slugs!

--- pages/events/[eventSlug].tsx
+++ pages/events/[eventSlug].tsx
@@ -1,11 +1,13 @@
 import { TopBarLayout } from "@/components/ApplicationLayouts/TopBarLayout";
+import { supabase } from "@/features/Auth/supabase";
 import { EventDetail } from "@/features/EventDetail";
-import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { ReactElement } from "react";
 
-export default function EventPage() {
-  const router = useRouter();
-  const { eventSlug } = router.query;
+// TODO: Devolver a la version anterior cuando nos auspicie vercel
+export default function EventPage({ eventSlug }: { eventSlug: string }) {
+  // const router = useRouter();
+  // const { eventSlug } = router.query;
 
   return (
     <>
@@ -16,4 +18,23 @@ export default function EventPage() {
 
 EventPage.getLayout = function getLayout(page: ReactElement) {
   return <TopBarLayout>{page}</TopBarLayout>;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await supabase.from("events").select("slug");
+  const paths = data?.map(el => {
+    return { params: { eventSlug: el.slug } };
+  }) as any;
+  return {
+    paths,
+    fallback: true, // false or "blocking" // See the "fallback" section below
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  return {
+    props: {
+      eventSlug: params!.eventSlug,
+    },
+  };
 };
