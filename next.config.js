55fee9500e68ba29c9f7b5047d64904bb1e1a8e5
Felipe Torres <ftorres@brex.com>
2022-07-13 19:13:24 -0700

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    "NEXT_PUBLIC_APP_ENV": process.env.APP_ENV || "local",
+    "NEXT_PUBLIC_APP_ENV": process.env.APP_ENV,
     "NEXT_PUBLIC_APP_VERSION": process.env.CF_PAGES_COMMIT_SHA,
     "NEXT_PUBLIC_APP_HOST": process.env.APP_HOST,
     "NEXT_PUBLIC_SUPABASE_API_URL": process.env.SUPABASE_API_URL,
@@ -11,7 +11,7 @@ const nextConfig = {
     "NEXT_PUBLIC_UNSPLASH_ACCESS_KEY": process.env.UNSPLASH_ACCESS_KEY,
   },
   reactStrictMode: true,
-  experimental: {
+  compiler: {
     emotion: true,
   },
   images: {
