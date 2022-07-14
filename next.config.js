6ba58d2d6259f9bd155367f12f5308d475100898
Felipe Torres <ftorres@brex.com>
2022-07-13 19:09:19 -0700

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    "NEXT_PUBLIC_APP_ENV": process.env.APP_ENV,
+    "NEXT_PUBLIC_APP_ENV": process.env.APP_ENV || "local",
     "NEXT_PUBLIC_APP_VERSION": process.env.CF_PAGES_COMMIT_SHA,
     "NEXT_PUBLIC_APP_HOST": process.env.APP_HOST,
     "NEXT_PUBLIC_SUPABASE_API_URL": process.env.SUPABASE_API_URL,
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
-export const dashboardMainURL = (
-  process.env.NEXT_PUBLIC_APP_ENV === "staging" && typeof window !== "undefined"
-    ? // IF this is "staging" it means we are on a pages.dev env.
-      window.location.origin
-    : process.env.NEXT_PUBLIC_APP_HOST!
-).replace(
-  /\/\s*$/,
-  "",
-) + "/";
+export const dashboardMainURL = (typeof window !== "undefined"
+  ? // IF this is "staging" it means we are on a pages.dev env.
+    window.location.origin
+  : process.env.NEXT_PUBLIC_APP_HOST!).replace(
+    /\/\s*$/,
+    "",
+  ) + "/";
